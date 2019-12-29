b2bb2491f24b2f1ba88a922001bf8442080c1443
Eugene Dvoretsky <radioxoma@gmail.com>
2019-12-29 19:11:35 +0300

    Better name for OS X

--- build_osx.sh
+++ build_osx.sh
@@ -2,9 +2,9 @@ echo Mac OS X build script invoked
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 BASENAME="heval_$TIMESTAMP"
 echo "pyinstaller target name $BASENAME"
-pipenv run pyinstaller --noconsole heval/__main__.py --name $BASENAME
+pipenv run pyinstaller --noconsole heval/__main__.py --name Heval
 sleep 1
-hdiutil create "dist/$BASENAME.dmg" -srcfolder "dist/$BASENAME.app" -ov
+hdiutil create "dist/$BASENAME.dmg" -srcfolder "dist/Heval.app" -ov
 echo Remove heval directory
 rm -rfv dist/heval
 echo Remove heval.app directory

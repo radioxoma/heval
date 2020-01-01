08d8bf693c7885cccc09fcc5282c814991e95348
Eugene Dvoretsky <radioxoma@gmail.com>
2020-01-01 16:22:11 +0300

    Updating choco python dep

--- build_osx.sh
+++ build_osx.sh
@@ -5,9 +5,9 @@ echo "target dmg name $BASENAME.dmg"
 pipenv run pyinstaller --noconsole heval/__main__.py --name Heval
 sleep 1
 hdiutil create "dist/$BASENAME.dmg" -srcfolder "dist/Heval.app" -ov
-tree dist/
 echo Remove heval directory
-rm -rf dist/heval
+rm -rf dist/Heval
 echo Remove Heval.app directory
 rm -rf dist/Heval.app
+tree dist/
 export OSX_FILE="dist/$BASENAME.dmg"

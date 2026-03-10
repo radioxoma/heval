52132d91f374f68913f46b2700a829d2bf3ede10
Eugene Dvoretsky <radioxoma@gmail.com>
2026-03-10 22:55:25 +0300

    Event-driven pyscript model update

--- docs/main.py
+++ docs/main.py
@@ -1,46 +1,62 @@
 #!/usr/bin/env python
 
 """
-https://radioxoma.github.io/sandbox
+Heval web interface (static website).
+
+PyCharm `RMB > Show Interpreter Paths`, Add pyscript stubs:
+https://github.com/pyscript/pyscript-stubs/tree/main/src/pyscript-stubs
 """
 
-from pyscript import web, when, Event
+from pyscript import Event, web, when, window
 import heval.common
 import heval.human
 
 
 human = heval.human.HumanModel()
 human_model_changed = Event()
+event_change = window.Event.new("change")  # Dummy object to generate events
 
 
-@when("change", "#sel_body_sex")
-def set_model_sex():
-    human.body_sex = heval.common.HumanSex[web.page["sel_body_sex"].value.upper()]
+@when("change", "select#body_sex")
+def select_changed(event):
+    print(f"select_changed {event.target.id} by event")
+    setattr(
+        human,
+        event.target.id,
+        heval.common.HumanSex[web.page["body_sex"].value.upper()],
+    )
     human_model_changed.trigger(None)
 
 
-@when("change", "#sbx_body_height")
-def set_model_height():
-    human.body_height = float(web.page["sbx_body_height"].value)
+@when("change", "input[type=number]")
+def spinbox_changed(event):
+    # if event.target.tagName == "INPUT" and event.target.type == "number":
+    print(f"spinbox_changed by event: {event.target.id}={event.target.value}")
+    setattr(human, event.target.id, float(event.target.value))
     human_model_changed.trigger(None)
 
 
-@when("click", "#btn_body_reset")
+@when("click", "#body_reset")
 def set_input_defaults():
-    web.page["sel_body_sex"].value = heval.common.HumanSex.MALE.name.lower()
-    set_model_sex()
-    web.page["sbx_body_height"].value = 1.86
-    set_model_height()
+    print("Reset form")
+    # Reset value selector
+    web.page["body_sex"].value = web.page["body_sex option:first-of-type"].value
+    web.page["body_sex"].dispatchEvent(event_change)
+    # Reset spinboxes
+    web.page["body_height"].value = 1.77
+    for k in web.page.find("input[type=number]"):
+        k.dispatchEvent(event_change)
+    human_model_changed.trigger(None)
 
 
 set_input_defaults()
 
 
-@when("click", "#btn_body_set")
 @when(human_model_changed)
 def eval_model():
     human.init()
-    web.page["output"].innerHTML = human.eval_body()
+    web.page["output_body"].innerHTML = human.eval_body()
+    web.page["output_labs"].innerHTML = human.eval_labs()
 
 
-web.page["output"].innerText = "Ready"
+web.page["output_body"].innerText = "Ready"
